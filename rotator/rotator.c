170feb64f241afeebd7e148cc12b822b7444b086
Vasily A <h4lf@rambler.ru>
2016-02-21 03:17:26 +0300

    zero step

---
+++ rotator/rotator.c
@@ -0,0 +1,17 @@
+/*
+ * rotator.c
+ *
+ * Created: 20.02.2016 20:54:31
+ *  Author: UB4LAG
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
